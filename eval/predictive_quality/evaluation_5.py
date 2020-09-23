95f66d696bb901dd8a84d360d6daecf5d5eb8d1c
MaStFAU <matthias.stierle@fau.de>
2020-09-23 15:38:25 +0200

    flag: log model in cloud only if < 1 GB

--- eval/predictive_quality/evaluation_5.py
+++ eval/predictive_quality/evaluation_5.py
@@ -1,7 +1,5 @@
 """
 Evaluation No. 5
-
-
 """
 
 from eval.evaluation import run_experiment
@@ -13,7 +11,7 @@ name_of_case_id = "CASE_KEY"
 name_of_activity = "ACTIVITY"
 name_of_timestamp = "EVENTTIMESTAMP"
 name_of_label = "EXCEPTION"
-hyper_params = {'num_epochs': 3,
+hyper_params = {'num_epochs': 5,
                 'batch_size': 1024,
                 'hidden_size': 2000}
 k = 10
@@ -22,4 +20,4 @@ log = preprocessing.import_data("../data", logfile, separator=",", quote='"', ca
                                 activity=name_of_activity,
                                 time_stamp=name_of_timestamp, target=name_of_label)
 
-run_experiment(log, hyper_params=hyper_params, k=k, ml_flow_run_name_prefix=logfile)
+run_experiment(log, hyper_params=hyper_params, k=k, ml_flow_run_name_prefix=logfile, save_artifact=False)
